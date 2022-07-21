5a776d2bff25b20be248ac1f44fb66d259b3765e
bruno arch <bluisfernandes@gmail.com>
2022-07-21 18:06:54 -0400

    constant secret key

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, session, request, url_for, redirect
-import secrets
+# import secrets
 
 
 app = Flask(__name__)
-app.secret_key = secrets.token_hex()
+# app.secret_key = secrets.token_hex()
+app.secret_key = 'hagsjdNCNJSHC_1'
 
 mc = dict()
 
