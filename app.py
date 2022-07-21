7b4d70b20c2187aecf434a81102745b47ce97f43
bruno arch <bluisfernandes@gmail.com>
2022-07-21 19:55:22 -0400

    add link to logout

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, session, request, url_for, redirect
 import os
 import secrets
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', secrets.token_urlsafe())
@@ -8,7 +11,7 @@ app.secret_key = os.getenv('SECRET_KEY', secrets.token_urlsafe())
 @app.route('/')
 def index():
     if 'username' in session:
-        return f'Logged in as {session["username"]}'
+        return f'Logged in as {session["username"]} <p><a href={url_for("logout")}>Logout</a></p>'
     return f"You are not logged in <a href={url_for('login')}>Login</a>"
 
 @app.route('/login/', methods=['GET', 'POST'])
