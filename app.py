ad9c9ed3fb56feedef0977b9c39fc28cb88f4331
bruno arch <bluisfernandes@gmail.com>
2022-07-20 22:53:11 -0400

    fix memcache and add some routes

--- app.py
+++ app.py
@@ -15,13 +15,23 @@ mc.enable_retry_delay(True)  # Enabled by default. Sets retry delay to 5s.
 
 @app.route("/")
 def home():
-    mc.set("foo", "bar")
+    mc.set("foo", "barra")
     return mc.get("foo", 'não tem')
 
 @app.route("/q")
 def q():
-    mc.set("foo", "bar")
+    mc.set("foo", "barra Q")
     return mc.get("foo", 'não tem Q')
+
+@app.route("/qq")
+def qq():
+    resp = mc.get("foo", 'não tem QQ1')
+    return f'/ qq \t{resp}'
+
+@app.route("/qqq")
+def qq2():
+    resp = mc.get("foo", 'não tem QQ2')
+    return f'/ qqq \t{resp}'
 
 if __name__ == "__main__":
     app.run()
